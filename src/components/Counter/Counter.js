3903811c2e040dcf3161784fea0847afb14351ae
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2021-04-16 14:16:50 +0700

    tdd

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Counter.css';
 
 const Counter = () => {
+  const [counter, setCounter] = useState(0);
+  const [input, setInput] = useState(1);
   return (
     <div>
       <h3 data-testid='header'>My Counter</h3>
+      <h2 data-testid='counter'>{counter}</h2>
+      <button data-testid='subtract-btn'>-</button>
+      <input
+        type='number'
+        data-testid='input'
+        value={input}
+        className='text-center'
+      />
+      <button data-testid='add-btn'>+</button>
     </div>
   );
 };
